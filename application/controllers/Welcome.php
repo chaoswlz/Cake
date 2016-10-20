8d851e3881fe241246a647d2b57934ad87134234
chaoswlz <lizewu@localhost>
2016-10-20 15:00:07 -0700

    job1 done

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -17,29 +17,21 @@ class Welcome extends Application
 	 * map to /welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
+        public function index() {
             $result = '';
             $oddrow = true;
             foreach ($this->categories->all() as $category) {
                 $viewparms = array(
                     'direction' => ($oddrow ? 'left' : 'right')
                 );
-                $viewparms = array_merge($viewparms,$category);
+                $viewparms = array_merge($viewparms, (array)$category);
+                $category->direction = ($oddrow ? 'left' : 'right');
                 $result .= $this->parser->parse('category-home', $category, true);
                 $oddrow = ! $oddrow;
             }
             $this->data['content'] = $result;
             $this->render();
-	}
-        
-        function render($template = 'template')
-        {
-            // use layout content if provided
-            $this->data['navbar'] = $this->parser->parse('navbar', $this->data, true);
-            if (!isset($this->data['content']))
-                $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
-            $this->parser->parse($template, $this->data);
-        }
+        }     
+
 
 }
