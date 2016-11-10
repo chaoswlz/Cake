5e8657881989ce9720c9782e1e3c2202260c461b
chaoswlz <lizewu@localhost>
2016-11-10 15:19:50 -0800

    step 3 and 4 done

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -14,6 +14,7 @@
 class Crud extends Application{
     function __construct() {
         parent::__construct();
+        $this->load->helper('formfields');
     }
     
     public function index() {
@@ -26,6 +27,28 @@ class Crud extends Application{
 
         $this->data['pagebody' ] ='mtce';
         $this->data['items'] = $this->menu->all();
+        $this->render();
+    }
+    
+    function edit($id=null) {    // try the session first    
+        $key = $this->session->userdata('key');    
+        $record = $this->session->userdata('record');   
+         // if not there, get them from the database   
+         if (empty($key)) {            
+            $record = $this->menu->get($id);           
+            $key = $id;           
+            $this->session->set_userdata('key',$id);            
+            $this->session->set_userdata('record',$record);   
+        }
+        // build the form fields
+        $this->data['fid'] = makeTextField('Menu code', 'id', $record->id);
+        $this->data['fname'] = makeTextField('Item name', 'name', $record->name);
+        $this->data['fdescription'] = makeTextField('Description', 'description', $record->description);
+        $this->data['fprice'] = makeTextField('Price, each', 'price', $record->price);
+        $this->data['fpicture'] = makeTextField('Item image', 'picture', $record->picture);
+        $this->data['fcategory'] = makeTextField('Category', 'category', $record->category);
+        // show the editing form
+        $this->data['pagebody'] = "mtce-edit";
         $this->render();
     }
 }
